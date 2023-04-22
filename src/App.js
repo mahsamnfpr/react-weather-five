febda8e72fd99a872824c0f6655003a199ea2ffe
mahsamnfpr <m.manafpoor@gmail.com>
2023-04-22 14:56:34 +0430

    add html for weather form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-
-      <button className="btn btn-primary">weather app</button>
+      <Weather/>
       <footer>
       This project was coded by Mahsa Manafpour and <a href='https://github.com/mahsamnfpr/react-weather-five' target="_blank">open-sourced on GitHub</a>
       </footer>
