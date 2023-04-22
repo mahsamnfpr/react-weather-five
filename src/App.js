d8ee37a12af6ce1a9aee08620f383870c47f0a08
mahsamnfpr <m.manafpoor@gmail.com>
2023-04-22 14:36:11 +0430

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      weather app
-      <p>
-      This App coded by Mahsa Manafpour and <a href='https://github.com/mahsamnfpr/react-weather-five'>open source</a>
-      </p>
+      <div className="container">
+
+      <button className="btn btn-primary">weather app</button>
+      <footer>
+      This project was coded by Mahsa Manafpour and <a href='https://github.com/mahsamnfpr/react-weather-five' target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
+      </div>
+    
   );
 }
-
-export default App;
