c1b6ce7927669f7c6f0b1570c17ed183aec12e7b
mahsamnfpr <m.manafpoor@gmail.com>
2023-04-28 18:55:52 +0430

    fix image problem

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,7 +15,7 @@ export default function WeatherInfo(props){
                 <div className="row" mt-3>
                     <div className="col-6">
                         <div className="clearfix">
-                        <img src="{props.data.iconUrl}" alt={props.data.description} className="float-left"/>
+                        <img src={props.data.iconUrl} alt={props.data.description} className="float-left"/>
                         <div className="float-left d-inline-block">
                         <span className="temperature">{Math.round(props.data.temperature)}</span>
                         <span className="unit">°C</span>
